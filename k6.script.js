944ce68efcccf144ab54d92a54814678a65a022e
Yevhen Laichenkov <elaichenkov@gmail.com>
2024-01-29 13:37:29 +0100

    initial commit

---
+++ k6.script.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 5 },
+  ],
+};
+
+export default function () {
+  const res = http.get('https://httpbin.test.k6.io/');
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+}
